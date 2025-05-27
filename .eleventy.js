4b936b7bea8234d9a725a5e636a7585b0e2059b3
Neofito89 <sergio.ale.v@gmail.com>
2025-05-27 02:54:24 +0200

    footer y header para pc

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addGlobalData("year", new Date().getFullYear());
+  eleventyConfig.addShortcode("inlineSVG", function(filePath) {
+    const fullPath = path.join(__dirname, 'src', filePath); // Ajusta 'src' si tu carpeta fuente es otra
+    try {
+      return fs.readFileSync(fullPath, 'utf8');
+    } catch (err) {
+      console.error(`Error leyendo SVG: ${fullPath}`, err);
+      return ''; // Retorna cadena vacía si falla la lectura
+    }
+  });
 
   return {
     dir: {
       input: "src",
       includes: "_includes",
+      layouts: "_includes/layouts",
       output: "dist"
     },
     markdownTemplateEngine: "njk",
--- src/index.md
+++ src/index.md
@@ -1,10 +1,5 @@
 ---
 layout: base.njk
-title: Home
 ---
 
-# Hello from Eleventy + GitHub
-
-This is your new Eleventy site served via GitHub Pages!
-
-y ahora voy a poner yo mis mierdas, empezando por el puto footpager
+# SERGIO ALEGRE
